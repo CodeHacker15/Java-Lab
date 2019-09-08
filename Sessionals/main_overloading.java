d486c0949b6b537411dbcb6744d8f9cd662f32e0
CodeHacker15 <189201053.siddharth@gmail.com>
2019-09-08 23:24:34 +0530

    a

---
+++ Sessionals/main_overloading.java
@@ -0,0 +1,15 @@
+class test
+{
+	public static void main(int a)
+	{
+		System.out.print(a);
+	}
+	public static void main(String arg[])
+	{
+		int i;
+		for(i=0;i<arg.length;i++)
+		{
+			System.out.print(arg[i]);
+		}
+	}
+}
