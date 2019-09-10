043897336a301ec3d8e548b90611346f79484fc4
CodeHacker15 <189201053.siddharth@gmail.com>
2019-09-10 12:48:54 +0530

    a

---
+++ Questions Random/main.java
@@ -0,0 +1,18 @@
+public class main
+{
+	public static void main(String args[])
+	{
+		Integer a = 69;
+		Integer b = 69;
+		System.out.println(a==b);
+		Integer c = 696;
+		Integer d = 696;
+		System.out.println(c==d);
+		Integer e = 42;
+		Integer f = 42;
+		System.out.println(e==f);
+		Integer g = 127;
+		Integer h = 127;
+		System.out.println(g==h);
+	}
+}
